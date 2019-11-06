a6a771ea18fcdcf05765a7904278a848365df222
esikkala <esko.ikkala@aalto.fi>
2019-11-06 17:12:46 +0200

    Update public path

--- webpack.client.common.js
+++ webpack.client.common.js
@@ -23,7 +23,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, outputDirectory),
     //publicPath: '/',
-    publicPath: '/sotasurmat',
+    publicPath: '/sotasurmat/',
   },
   module: {
     rules: [
