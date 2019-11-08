c9afd04e4764cdc1d86ab864a65f9054004e929e
esikkala <esko.ikkala@aalto.fi>
2019-11-08 16:13:07 +0200

    Use https in api url

--- src/client/epics/index.js
+++ src/client/epics/index.js
@@ -38,7 +38,7 @@ import { rootUrl } from '../configs/config';
 
 export const apiUrl = (process.env.NODE_ENV === 'development')
   ? `http://localhost:3001${rootUrl}/api/`
-  : `http://${location.hostname}${rootUrl}/api/`;
+  : `https://${location.hostname}${rootUrl}/api/`;
 
 const backendErrorText = `Ei pystytty yhdistämään tietokantaan.
 Sivusto toimii oikein vain http yhteyden kautta ja https osoitteen alussa aiheuttaa tämän virheen.
