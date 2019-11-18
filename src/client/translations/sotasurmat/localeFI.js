1f5960da67109e6f992177c99a09be3be0e40bac
Heikki <heikki.rantala@aalto.fi>
2019-11-18 09:53:23 +0200

    add instructions in Finnish

--- src/client/translations/sotasurmat/localeFI.js
+++ src/client/translations/sotasurmat/localeFI.js
@@ -282,13 +282,119 @@ export default {
     },
   },
   instructions: `
-    <h1 class="MuiTypography-root MuiTypography-h2 MuiTypography-gutterBottom">
-      Ohjeet
-    </h1>
-    <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+        <h1 class="MuiTypography-root MuiTypography-h2 MuiTypography-gutterBottom">
+          Ohjeet
+        </h1>
+        <h2 class="MuiTypography-root MuiTypography-h4 MuiTypography-gutterBottom">
+          Tiedonhaku
+        </h2>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          Tietokannan hakutoiminnot pohjautuvat <a href="https://doi.org/10.2200/S00190ED1V01Y200904ICR005" target='_blank' rel='noopener noreferrer'>fasettihakuun</a>.
+          Oletusarvoisesti jokainen hakuominaisuus näyttää kaikki tulokset omasta luokastaan.
+          Toisin sanottuna alkuasetelmassa hakutulosten listaus näyttää koko tietokannan sisällön.
+          Tätä hakutulosten joukkoa voi rajata pienemmäksi tekemällä valintoja vasemman reunan suodattimia ja muita hakuominaisuuksia käyttämällä.
+        </p>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+        Suodattimen kaikki mahdolliset arvot ovat esillä listana tai, jos mahdollista, hierarkisena puurakenteena. Kunkin arvon perässä esitetään se lukumäärä kuinka monta kertaa arvo esiintyy koko aineistossa (tai jo muiden suodattimien pohjalta tehdyssä rajauksessa).
+        Suodattimesta on mahdollista valita useita arvoja samaan aikaan.
+        Suodattimen valintoja on mahdollista järjestää joko arvojen määrän tai aakkosjärjestyksen mukaan.
+        Valinnan poistaminen onnistuu klikkaamalla sitä uudestaan listalta.
+        Kaikki valinnat voi poistaa hakupalkin ylälaidasta
+        </p>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          Fasettihaun tehokkuus tulee esiin useampia suodattimia käytettäessä.
+          Valittaessa useampia suodattimia samanaikaisesti, hakutulos rajautuu niin, että tuloksina näytetään vain ne henkilöt, jotka täyttävät kaikki valitut ehdot.
+          Voit tehdä myös saman suodattimen sisällä useita valintoja, jolloin tulokseksi tulevat sen suodattimen osalta kaikki henkilöt, jotka täyttävät jonkun tehdyn valinnan.
+          Esimerkiksi kun valitset “kirjoillaolokunta” suodattimesta “Helsinki” ja “Tampere”,
+           sekä valitset “Osapuoli” suodattimesta “Punainen” saat tulosjoukoksi kaikki henkilöt joiden osapuoleksi on määritetty punainen JA kirjoillaolokunnaksi joko Helsinki TAI Tampere.
+        </p>
+        <h2 class="MuiTypography-root MuiTypography-h4 MuiTypography-gutterBottom">
+          Sotasurmat
+        </h2>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          Sotasurmat-näkymä sisältää viisi erilaista alinäkymää joilla suodattimien rajaamaa tulosjoukkoa voi tarkastella: Taulukko, Piirakkakaavio, Viivakaavio, Kartta sekä CSV-vienti.
+          Kaavioita ei tule pitää mitenkään virallisina, vaan niitä kannattaa käyttää suuntaa-antavina visualisointeina.
+        </p>
+        <h3 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom">
+          Taulukko
+        </h3>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          Taulukko on oletusnäkymä, joka näyttää taulukkona suodattamien määräämän tulosjoukon mukaisia henkilöitä sekä häiden keskeisiä ominaisuuksiaan.
+          Taulukko näyttää sivulla vain määrätyn määrän henkilöitä. Taulukkos voi selata yläosassa olevilla nuolipainikkeilla.
+          Henkilöt voi järjestää haluamansa ominaisuuden mukaan painamalla taulukon yläosassa olevaa ominaisuuden nimeä. Oletuksena henkilöt järjestetään sukunimen perusteella.
+          Taulukko näyttää henkilön tiedot siinä muodossa kuin niitä käytetään haussa. Klikkaamalla henkilön nimeä pääset henkilön omalle sivulle.
+          Henkilön omalla sivulla on paljon enemmän tietoa henkilöstä, sekä mahdollisia vaihtoehtoisia tietoja joita on saatu eri lähteistä.
+          Haku käyttää näistä tiedoista vain ensisijaiseksi määriteltyä.
+        </p>
+        <h3 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom">
+          Piirakkakaavio
+        </h3>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          Visualisoi valitun ominaisuuden suhteellisia määriä piirakkakaaviona.
+        </p>
+        <h3 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom">
+          Viivakaavio
+        </h3>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          Viivakaavio-alinäkymässä voit visualisoida rajattujen henkilöiden ikää, syntymävuotta tai kuolinpäivää viivakaavion avulla.
+          Iälle ja syntymävuodelle lasketaan myös keskiarvo ja mediaani vuoden tarkkuudella.
+          Huomaa, että kuolinpäivä kaavio näyttää tietoa vain henkilöistä, joille on tietokannassa merkitty tarkka kuolinpäivä.
+        </p>
+        <h3 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom">
+          Kartta
+        </h3>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          Tässä alinäkymässä voit etsiä kuolleita klusteroidun kartan avulla. Kartta näyttää henkilöitä kuolinkunnan perusteella.
+          Huomaa, että kunnat on yhdistetty koordinaatteihin koneellisesti, joten koordinaateissa on virheitä ja useita kuntia puuttuu varsinkin ulkomailta.
+        </p>
+        <h3 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom">
+          CSV
+        </h3>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+        Henkilöiden tärkeimmät tiedot on mahdollista ladata CSV-muodossa. Tiedosto ei sisällä vaihtoehtoisia tietoja, lähteitä eikä lisätietoja.
+        CSV-tiedosto on mahdollista avata taulukkolaskentaohjelmassa, mutta merkistökoodaus ja erotin on määritettävä oikein.
+        Microsoft Excelia käytettäessä se ei välttämättä ole ilmeistä. Hyödylliset ohjeet merkistökoodauksen muuntamiseksi löydät
+        <a href="https://support.office.com/fi-fi/article/ohjattu-tekstin-tuominen-c5b02af6-fda1-4440-899f-f78bafe41857#ID0EAAFAAA=Office_2010_-_Office_2016" target='_blank' rel='noopener noreferrer'>täältä</a>.
+        Seuraa ohjeita ja valitse tiedoston alkuperäksi 65001: Unicode (UTF-8).
+        Seuraavassa kohdassa valitse erottimeksi pilkku, ja paina valmis-painiketta.
+        Jos käytät esimerkiksi LibreOfficen taulukkolaskentaohjelmaa, ohjelman pitäisi kysyä merkistökoodausta ja erotinta heti tiedostoa avattaessa.
+        Valitse myös siinä UTF-8 koodaus ja erottimeksi pilkku.
+        </p>
+        <h2 class="MuiTypography-root MuiTypography-h4 MuiTypography-gutterBottom">
+          Taistelut
+        </h2>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+        Taistelut -näkymä perustuu Kansallisarkistossa säilytettävään entisen sota-arkiston työntekijöiden laatimaan
+        <a href= “http://wiki.narc.fi/portti/index.php/Sis%C3%A4llissodan_taistelupaikkakortisto” target='_blank' rel='noopener noreferrer'>Sisällissodan taistelupaikkakortistoon</a>.
+        Kortiston tiedot on muunnettu taulukkomuotoon ja taisteluille on haettu koordinaatit kartalla esittämistä varten.
 
-    </p
-  `,
+        Taisteluiden nimet ovat selkeästi valkoisesta näkökulmasta annettuja.
+        Näkymässä nimet näytetään siinä muodossa kun ne kortistossa on esitetty, ei kannanottona tapahtumiin.
+        Näkymän tarkoituksena on luoda helppo visuaalinen keino tutustua sisällissodan taisteluiden kulkuun.
+        </p>
+        <h3 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom">
+          Taulukko
+        </h3>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          Taulukon avulla voit selata taisteluden tietoja, ja järjestää niitä esimerkiksi alkamispäivän mukaan.
+        </p>
+        <h3 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom">
+          Kartta
+        </h3>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          klusteroidun kartan avulla voit tarkastella taisteluita kartalla. Kartta kerää lähellä toisiaan olevat paikat yhteen.
+          Klikkaamalla tällaista “klusteria” tai zoomaamalla tarpeeksi lähelle, saat näkyviin yksittäiset taistelupaikat.
+          Taistelupaikkojen koordinaatit on tallennettu vain kahden desimaalin tarkkuudella, joten ne eivät välttämättä ole tismalleen oikeassa paikassa.
+        </p>
+        <h3 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom">
+          Animaatio
+        </h3>
+        <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+          Animaatio visualisoi taiteluita kartalla. Kun taistelu alkaa se vilahtaa punaisena pisteenä ja jää kartalle harmaana.
+          Animaatio alkaa oletuksena vuoden 1918 alusta, mutta kortisto sisältää myös useita vuoden 1917 puolella tahtuneita väkivaltaisuuksia.
+        </p>
+
+    `,
   feedback: `
     <h1 class="MuiTypography-root MuiTypography-h2 MuiTypography-gutterBottom">
       Palaute
