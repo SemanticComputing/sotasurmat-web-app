f1fd5c3b1d6a9283dd34ab06c49fd8a1095baf3f
esikkala <esko.ikkala@aalto.fi>
2019-11-18 11:48:46 +0200

    Reorder logos, add HELDIG logo

--- src/client/components/main_layout/Footer.js
+++ src/client/components/main_layout/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
-import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import aaltoLogo from '../../img/logos/aalto-logo-white-no-background-small.png';
+import uhLogo from '../../img/logos/university-of-helsinki-logo-white-no-background-small.png';
+import heldigLogo from '../../img/logos/heldig-logo-small.png';
 import secoLogo from '../../img/logos/seco-logo-white-no-background-small.png';
-import arkistoLogo from '../../img/logos/ka-tunnus-fi-white.png';
+// import arkistoLogo from '../../img/logos/ka-tunnus-fi-white.png';
 
 
 const styles = theme => ({
@@ -28,70 +29,72 @@ const styles = theme => ({
     marginBottom: theme.spacing(1),
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
-    [theme.breakpoints.up(1500 + theme.spacing(6))]: {
-      width: 1500,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    [theme.breakpoints.up(1300 + theme.spacing(6))]: {
+      width: 1300,
       marginLeft: 'auto',
       marginRight: 'auto',
     },
   },
   link: {
-    textDecoration: 'none'
-  },
-  logoContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  secLogo: {
-    height: 46,
+    textDecoration: 'none',
+    marginLeft: theme.spacing(4),
+    marginRight: theme.spacing(4)
   },
   aaltoLogo: {
     height: 39,
   },
-  arkistoLogo: {
-    height: 44,
+  uhLogo: {
+    height: 52,
   },
+  secoLogo: {
+    height: 46,
+  },
+  heldigLogo: {
+    height: 44,
+  }
 });
 
 const Footer = props => {
   const { classes } = props;
   return (
     <Paper className={classes.root}>
-      <Grid container className={classes.layout}>
-        <Grid container spacing={3} item xs={12}>
-          <Grid item xs className={classes.logoContainer}>
-            <a
-              className={classes.link}
-              href="https://seco.cs.aalto.fi/"
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <img className={classes.secLogo} src={secoLogo} alt='logo' />
-            </a>
-          </Grid>
-          <Grid item xs className={classes.logoContainer}>
-            <a
-              className={classes.link}
-              href="https://www.arkisto.fi/"
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <img className={classes.arkistoLogo} src={arkistoLogo} alt='logo' />
-            </a>
-          </Grid>
-          <Grid item xs className={classes.logoContainer}>
-            <a
-              className={classes.link}
-              href="https://www.aalto.fi/"
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <img className={classes.aaltoLogo} src={aaltoLogo} alt='logo' />
-            </a>
-          </Grid>
-
-        </Grid>
-      </Grid>
+      <div className={classes.layout}>
+        <a
+          className={classes.link}
+          href="https://www.aalto.fi/"
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img className={classes.aaltoLogo} src={aaltoLogo} alt='logo' />
+        </a>
+        <a
+          className={classes.link}
+          href="https://seco.cs.aalto.fi/"
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img className={classes.secLogo} src={secoLogo} alt='logo' />
+        </a>
+        <a
+          className={classes.link}
+          href="https://www.helsinki.fi/"
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img className={classes.uhLogo} src={uhLogo} alt='logo' />
+        </a>
+        <a
+          className={classes.link}
+          href="http://www.heldig.fi/"
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img className={classes.aaltoLogo} src={heldigLogo} alt='logo' />
+        </a>
+      </div>
     </Paper>
   );
 };
